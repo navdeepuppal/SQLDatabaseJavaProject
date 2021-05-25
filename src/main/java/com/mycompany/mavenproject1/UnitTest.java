1290a5bd5de4dfd4d5dc3c3504b40fa292222b16
navdeepuppal <navdeepuppal1609@gmail.com>
2021-05-26 01:09:48 +0530

    jar

---
+++ src/main/java/com/mycompany/mavenproject1/UnitTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hello i am from app test named as UnitTest ! ");
+	}
+
+}
